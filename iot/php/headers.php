b1650b13f89f2c26f6d71bafb9a40bdba95b554a
migsbernaldo <migsbernaldo@gmail.com>
2024-02-20 06:08:37 +0700

    header.php change

--- iot/php/headers.php
+++ iot/php/headers.php
@@ -3,7 +3,10 @@
 header("Cache-Control: no-store, must-revalidate");
 header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
 # Access-Control Headers
-header("Access-Control-Allow-Origin: http://iot.comteq.edu.ph, http://iot.comteq.edu.ph:50001, http://172.16.2.250, http://172.16.2.252");
+#header("Access-Control-Allow-Origin: http://iot.comteq.edu.ph, http://iot.comteq.edu.ph:50001, http://172.16.2.250, http://172.16.2.252");
+
+
+header("Access-Control-Allow-Origin: http://192.168.0.108, http://192.168.0.105");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-?>
+?>  
