7db5e9be09a7c8be68b735bbf99393cd4748a16f
migsbernaldo <migsbernaldo@gmail.com>
2024-02-29 15:39:23 +0800

    2.2

--- iot/php_insert/insertLights.php
+++ iot/php_insert/insertLights.php
@@ -13,8 +13,46 @@ if ($result->num_rows > 0) {
     // Alternate the activity ID based on the last inserted value
     if ($lastActivityID == 3) {
         $activityID = 4;
+
+            $arduino_ip = '192.168.1.120';
+            $arduino_port = 8080; // Change this to the port your Arduino is listening on
+
+            // Data to send to the Arduino
+            $data = 'Lights: 2';
+
+            // Create a TCP/IP socket connection to the Arduino
+            $socket = fsockopen($arduino_ip, $arduino_port, $errno, $errstr, 10);
+            if (!$socket) {
+                echo "Error: $errstr ($errno)<br>";
+            } else {
+                // Send the data to the Arduino
+                fwrite($socket, $data);
+                fclose($socket);    
+                echo "Data sent to Arduino: $data<br>";
+            }
+
+
     } else {
         $activityID = 3;
+
+        $arduino_ip = '192.168.1.120';
+        $arduino_port = 8080; // Change this to the port your Arduino is listening on
+
+        // Data to send to the Arduino
+        $data = 'Lights: 1';
+
+        // Create a TCP/IP socket connection to the Arduino
+        $socket = fsockopen($arduino_ip, $arduino_port, $errno, $errstr, 10);
+        if (!$socket) {
+            echo "Error: $errstr ($errno)<br>";
+        } else {
+            // Send the data to the Arduino
+            fwrite($socket, $data);
+            fclose($socket);    
+            echo "Data sent to Arduino: $data<br>";
+        }
+
+
     }
 }
 $sql2 = "INSERT INTO logs(UserID, ActivityID, LogText, Timestamp, Date, Time)
